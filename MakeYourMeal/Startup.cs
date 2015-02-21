f6d278f6180de47d175ada4547743c5f918fd889
Issa Haddar <issahaddar5@gmail.com>
2015-02-21 12:43:31 +0200

    first commit

---
+++ MakeYourMeal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MakeYourMeal.Startup))]
+namespace MakeYourMeal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
